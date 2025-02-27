62ade0f5b55ed276b0b0e5d0b2679f7583f94174
Abdihakim <shiinecadde17@gmail.com>
2025-02-27 23:09:49 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import NutritionProvider from './Nutritioncontext'
+import NutritionProvider from './NutritionContext'
 import App from './App'
 
 
