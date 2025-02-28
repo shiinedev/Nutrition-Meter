7efa96836507289e053c32a432c47f7b911d219e
shiinedev <shiinecadde17@gmail.com>
2025-02-28 17:28:02 +0300

    changed title name

--- src/components/NutritionApp.jsx
+++ src/components/NutritionApp.jsx
@@ -5,7 +5,7 @@ import NutritionList from './NutritionList'
 const NutritionApp = () => {
   return (
     <div className='bg-zinc-900  min-h-screen p-4 lg:p-6 text-zinc-100'>
-      <h1 className='text-3xl  text-center font-bold'>NutritionApp</h1>
+      <h1 className='text-3xl  text-center font-bold'>Nutrition Meter</h1>
       <NutritionForm />
       <NutritionList />
     </div>
